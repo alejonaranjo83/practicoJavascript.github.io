5949c71726fed870634fd1e56076654ce10a2978
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-01-27 16:37:37 -0500

    Escuchando eventos opción 1: desde HTML

--- script.js
+++ script.js
@@ -9,11 +9,10 @@
 
 // Usando "querySelector" me puedo conectar con las etiquetas de HTML utilizando lo mismo que uso en CSS:
 const h1 = document.querySelector("h1");
-// Este me muestra todas las etiquetas de ese tipo en un array
-const p = document.querySelectorAll("p");
-const parrafoClase = document.querySelector(".parrafo-clase");
-const parrafoId = document.querySelector("#parrafo-id");
-const input = document.querySelector("input")
+const input1 = document.querySelector("#calculo1")
+const input2 = document.querySelector("#calculo2")
+const btn = document.querySelector("#btnCalcular")
+const pResult = document.querySelector("#result")
 
 
 // En "branch" llamada "clase11" está el código hasta la clase 11, en la cual se estaban explorando posibilidades para escribir contenido desde JS a HTML. A partir de aquí, código se limpia para seguir con otras cosas de manera más organizada.
@@ -22,3 +21,16 @@ const input = document.querySelector("input")
 
 // 3. EVENTOS
 
+// Para escuchar eventos, puedo hacerlo desde el HTML o el JS. En el primer caso, existen opciones como "onclick / onchange / onsubmit" que asocio a fxnes creadas en JS en las que especifico qué quiero q suceda. Esto puede hacer que el HTML no sea tan limpio
+
+function btnOnClick() {
+    // console.log("Escuchando el evento de click")
+    
+    const sumaInputs = (Number(input1.value) + Number(input2.value))
+
+    pResult.innerText = "El resultado de la operación es " + sumaInputs
+}
+
+// function contentChange() {
+//     console.log(`Escuchando cambio en el input`)
+// }
