4e3223f434a1485c24caa7cda42e8d8ce1ebbbbc
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-01-27 15:48:53 -0500

    Se limpia HTML y JS para continuar explorando otras posibilidades

--- script.js
+++ script.js
@@ -15,58 +15,10 @@ const parrafoClase = document.querySelector(".parrafo-clase");
 const parrafoId = document.querySelector("#parrafo-id");
 const input = document.querySelector("input")
 
-// Si quiero ver el valor de un elemento en particular
-console.log(input.value)
 
-// Si quiero ver todos los elementos en la consola
-console.log({
-    h1,
-    p,
-    parrafoClase,
-    parrafoId,
-    input
-})
+// En "branch" llamada "clase11" está el código hasta la clase 11, en la cual se estaban explorando posibilidades para escribir contenido desde JS a HTML. A partir de aquí, código se limpia para seguir con otras cosas de manera más organizada.
 
 
 
-// 2. INSERTAR CONTENIDO EN HTML DESDE JS
+// 3. EVENTOS
 
-// h1.innerHTML = "Título cambiado desde JS"
-
-// Esta forma genera más peligros para los usuarios:
-// h1.innerHTML = "Título al que le meto una etiqueta <p>RIESGO</p> elemento desde JS"
-
-// Es más seguro usar:
-h1.innerText = "Título más seguro <p> MENOS RIESGO</p> desde JS"
-
-// Si quiero darle una clase al elemento HTML:
-console.log(h1.getAttribute("pantalla"))
-
-// Puedo modificar atributo de etiqueta del HTML (lo veo en "Elements" del navegador):
-h1.setAttribute("pantalla", "cambiado")
-
-// Puedo añadirle atributo:
-h1.classList.add("otro")
-h1.classList.add("otro2")
-
-// O removerlo:
-h1.classList.remove("otro")
-
-// También está ".toggle" y ".contains"
-
-// Añadir contenido a un input:
-input.value = "5678"
-
-
-// Si quiero crear un elemento desde 0 en JS:
-
-const img = document.createElement("img")
-
-img.setAttribute("src", "https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
-
-console.log(img)
-
-parrafoId.append(img)
-
-// Puedo eliminar contenido previo del HTML para q se quede solo lo q inserté desde JS
-parrafoId.innerText = " "
